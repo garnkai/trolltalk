95ff56eddd8c5069a46517dc4dfabb39795a4cd4
Zhe <122396273+LordZUL@users.noreply.github.com>
2024-03-23 20:29:49 -0400

    Changed Homepage URL orders and added a url link to the play button

--- trollproject/trollapp/views.py
+++ trollproject/trollapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+def homepage(request):
+    return render(request, "homepage.html")
 def lobby(request): # Added function
     if not request.user.is_authenticated:
         return redirect("login-user")
