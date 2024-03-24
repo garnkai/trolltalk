652a4d1275ab4eabf1cf2e4f504c6d6fc5ae5734
Leonardo Nicolas Bigentini <leniu2022@gmail.com>
2024-03-24 04:51:55 -0400

    Developed a lot of lobbies functionality, including joining a create lobby form, joining lobbies through code or by clicking a link. Created lobby models. Updated multiplayer chat code to work with a lobby code, so that multiple chats are independent

--- trollproject/trollapp/consumers.py
+++ trollproject/trollapp/consumers.py
@@ -1,3 +1,6 @@
+
+
+
 # File routes the WebSocket connections to the consumers
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -5,7 +8,12 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 # WebSocket Chat Code taken from https://www.geeksforgeeks.org/realtime-chat-app-using-django/ 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.roomGroupName = "group_chat_gfg"
+        #self.roomGroupName = "group_chat_gfg"
+
+        self.websocket_id = self.scope['url_route']['kwargs']['websocket_id']
+        self.roomGroupName = f'chat_{self.websocket_id}'
+
+
         await self.channel_layer.group_add(
             self.roomGroupName ,
             self.channel_name
@@ -20,13 +28,116 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         username = text_data_json["username"]
+        
         await self.channel_layer.group_send(
             self.roomGroupName,{
                 "type" : "sendMessage" ,
                 "message" : message , 
                 "username" : username ,
-            })
+            }
+        )
     async def sendMessage(self , event) : 
         message = event["message"]
         username = event["username"]
-        await self.send(text_data = json.dumps({"message":message ,"username":username}))
+        await self.send(text_data = json.dumps({
+            "message":message ,"username":username
+        }))
+
+
+
+
+'''
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username
+        }))
+'''
+
+'''
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.websocket_id = self.scope['url_route']['kwargs']['websocket_id']
+        self.room_group_name = f'chat_{self.websocket_id}'
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username
+        }))
+'''
--- trollproject/trollapp/models.py
+++ trollproject/trollapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Lobby(models.Model):
+    #lobby_id = models.CharField(max_length=50, unique=True)
+    lobby_name = models.CharField(max_length=50, unique=True)
+    num_players = models.IntegerField(default=4) 
+    game_mode = models.CharField(max_length=100, default='Floyd')  
+    privacy = models.CharField(max_length=20, default='public') 
+
+    def __str__(self):
+        return self.lobby_id
